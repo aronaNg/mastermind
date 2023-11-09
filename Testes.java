4f3a96ff491cc93f4607633cb21999fe886acd0e
StanouilleLaFripouille <146169132+StanouilleLaFripouille@users.noreply.github.com>
2023-11-09 16:52:11 +0100

    Add files via upload

--- Testes.java
+++ Testes.java
@@ -11,10 +11,10 @@ public class Testes {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Button Grid Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(560, 840);
+        frame.setSize(440, 880);
 
         // Crée un JPanel avec un GridLayout
-        JPanel buttonPanel = new JPanel(new GridLayout(12, 7)); // grille de 12 lignes, 7 colonnes
+        JPanel buttonPanel = new JPanel(new GridLayout(16, 7)); // grille de 12 lignes, 7 colonnes
         buttonPanel.setPreferredSize(new Dimension(400, 800)); // Spécifiez la taille préférée en pixels
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
@@ -57,6 +57,7 @@ public class Testes {
         ImageIcon IL10C3 = new ImageIcon("Image\\L10C3.gif");
         ImageIcon IL10C5 = new ImageIcon("Image\\L10C5.gif");
         ImageIcon IL10C6 = new ImageIcon("Image\\L10C6.gif");
+        ImageIcon IL10C7 = new ImageIcon("Image\\L10C7.gif");
         ImageIcon IL10C8 = new ImageIcon("Image\\L10C8.gif");
         ImageIcon IL11C1 = new ImageIcon("Image\\L11C1.gif");
         ImageIcon IL11C8 = new ImageIcon("Image\\L11C8.gif");
@@ -80,31 +81,111 @@ public class Testes {
         
         ImageIcon[] imageIcons = {I0R0B,I1B,I1R,I1R1B,I1R2B,I1R3B,I2B,I2R,I2R1B,I2R2B, /*10*/I3B,I3R,I3R1B,I4B,I4R,IEntreBoutons,IL1C1,IL1C2,IL1C3,IL1C5, /*20*/IL1C6,IL1C7,IL1C8,IL2C1,IL2C6,IL2C8,IL3C1,IL3C6,IL3C8,IL9C1, /*30*/IL9C6,IL10C1,IL10C2,IL10C3,IL10C5,IL10C6,IL10C8,IL11C1,IL11C8,IL12C1, /*40*/IL12C2,IL12C3,IL12C7,IL12C8,IPBS,IPB,IPJS,IPJ,IPRS,IPR, /*50*/IPVS,IPV,ITS,IT,IBValideClic,IBValide};
         
+        java.awt.event.ActionListener buttonActionListener = new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                String actionCommand = e.getActionCommand();
+                System.out.println("Clicked: " + actionCommand);
+            }
+        };
+
+
         // Crée et ajoute des boutons à la grille
         int adaptateur;
-        for (int row = 0; row < 12; row++) {
+        for (int row = 0; row < 16; row++) {
             for (int col = 0; col < 8; col++) {
                 JButton button;
                 adaptateur = 0;
                 ImageIcon image;
-                if(col==6 && 0<row && row<9)
+                if(col==6 && 0<row && row<13)
                    image = imageIcons[0];
-                else if(col==5 && row==10)
-                    image =  imageIcons[15];
                 else if(row==0)
                 {
                     if(col>2)
                         adaptateur = -1;
                     image = imageIcons[16 + col + adaptateur];
                 }
-                //else if(col==0)
+                else if(col==0){
+                    if(row==1)
+                         image = IL2C1;
+                    else if(1<row && row<12)
+                         image = IL3C1;
+                    else if(row==12)
+                        image = IL9C1;
+                    else if(row==13)
+                         image = IL10C1;
+                    else if(row==14)
+                         image = IL11C1;
+                    else
+                         image = IL12C1;
+                }
+                else if(col==5 && row<13){
+                    if(row==1)
+                        image=IL2C6;
+                    else if(1<row && row<12)
+                        image=IL3C6;
+                    else
+                        image=IL9C6;
+                }
+                else if(col==7 && row!=13){
+                    if(row==1)
+                        image=IL2C8;
+                    else if(1<row && row<13)
+                        image=IL3C8;
+                    else if(row==14)
+                        image=IL11C8;
+                    else
+                        image=IL12C8;
+                }
+                else if(row==13){
+                    if(col==1)
+                        image=IL10C2;
+                    else if(col==2 || col==3)
+                        image=IL10C3;
+                    else if(col==4)
+                        image=IL10C5;
+                    else if(col==5)
+                        image=IL10C6;
+                    else if(col==6)
+                        image=IL10C7;
+                    else
+                        image=IL10C8;
+                }
+                else if(row==14){
+                    if(col==1)
+                        image=IPJ;
+                    else if(col==2)
+                        image=IPR;
+                    else if(col==3)
+                        image=IPV;
+                    else if(col==4)
+                        image=IPB;
+                    else if(col==5)
+                        image=IEntreBoutons;
+                    else 
+                        image=IBValide;
+                }
+                else if(row==15){
+                    if(col==1)
+                        image=IL12C2;
+                    else if(col==6)
+                        image=IL12C2;
+                    else
+                        image=IL12C3;
+                }        
                 else 
                     image = imageIcons[53];
 
-                ImageIcon scaledIcon = getScaledImageIcon(image, 70, 70);
-                button = new JButton(scaledIcon);
-            
 
+                ImageIcon scaledIcon = getScaledImageIcon(image, 53, 53);
+                button = new JButton(scaledIcon);
+                button.setActionCommand("Button " + (row * 8 + col + 1));
+                button.addActionListener(buttonActionListener);
+
+
+                if(col==6 && row==10)
+                    button.setPressedIcon( getScaledImageIcon(IBValideClic, 55, 55));
+                button.setBorderPainted(false); 
                 constraints.gridx = col;
                 constraints.gridy = row;
                 buttonPanel.add(button, constraints);
